9fde7f4da46537af69931093800d6513b224181c
Tony-Thinkpad-T440p <yong1236@foxmail.com>
2015-12-22 14:54:01 +0800

    -

---
+++ deploy-t440.sh
@@ -0,0 +1,4 @@
+grunt build
+cp -rf dist/bower_components /d/workspace/spark/cmcc-sns/src/main/webapp/static/admin
+cp -rf dist/scripts /d/workspace/spark/cmcc-sns/src/main/webapp/static/admin
+cp -rf dist/styles /d/workspace/spark/cmcc-sns/src/main/webapp/static/admin
