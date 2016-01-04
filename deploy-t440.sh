01a8e2e2bade626576c2400362a203968c8c6252
Tony-Thinkpad-T440p <yong1236@foxmail.com>
2016-01-04 18:07:04 +0800

    Add one login page.

--- deploy-t440.sh
+++ deploy-t440.sh
@@ -1,4 +1,8 @@
 grunt build
-cp -rf dist/bower_components /d/workspace/spark/cmcc-sns/src/main/webapp/static/admin
-cp -rf dist/scripts /d/workspace/spark/cmcc-sns/src/main/webapp/static/admin
-cp -rf dist/styles /d/workspace/spark/cmcc-sns/src/main/webapp/static/admin
+cp -rf dist/bower_components /d/workspace/eclipse/cmcc-sns/src/main/webapp/static/admin
+cp -rf dist/scripts /d/workspace/eclipse/cmcc-sns/src/main/webapp/static/admin
+cp -rf dist/styles /d/workspace/eclipse/cmcc-sns/src/main/webapp/static/admin
+
+cp -rf dist/bower_components /d/workspace/eclipse/parim-spark/spark-templates/src/main/resources/META-INF/resources/static/admin
+cp -rf dist/scripts /d/workspace/eclipse/parim-spark/spark-templates/src/main/resources/META-INF/resources/static/admin
+cp -rf dist/styles /d/workspace/eclipse/parim-spark/spark-templates/src/main/resources/META-INF/resources/static/admin
